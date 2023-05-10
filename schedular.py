e277087af5f0af2f8cd85ac5ed77b13724192545
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-10 10:39:05 +0330

    fix: bugs

--- schedular.py
+++ schedular.py
@@ -54,12 +54,14 @@ class Scheduler:
         Sort tasks based on the nearest deadline.
         """
         # get interrupts first
-        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()].sort(key=lambda x: x.deadline)
+        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()]
+        interrupts.sort(key=lambda x: x.deadline)
         # get other tasks
-        others = [task for task in self.task_set.get_all() if not task.is_interrupt()].sort(key=lambda x: x.deadline)
+        others = [task for task in self.task_set.get_all() if not task.is_interrupt()]
+        others.sort(key=lambda x: x.deadline)
         # set new tasks
         self.task_set.set_tasks(interrupts+others)
-    
+   
 
     def dm(self):
         """DM
@@ -67,9 +69,11 @@ class Scheduler:
         Sort tasks based on the deadline monotonic.
         """
         # get interrupts first
-        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()].sort(key=lambda x: x.deadline)
+        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()]
+        interrupts.sort(key=lambda x: x.deadline)
         # get other tasks by deadline monotonic
-        others = [task for task in self.task_set.get_all() if not task.is_interrupt()].sort(key=lambda x: 1/x.deadline, reverse=True)
+        others = [task for task in self.task_set.get_all() if not task.is_interrupt()]
+        others.sort(key=lambda x: 1/x.deadline, reverse=True)
         # set new tasks
         self.task_set.set_tasks(interrupts+others)
 
@@ -80,10 +84,13 @@ class Scheduler:
         Sort tasks based on the rate monotonic.
         """
         # get interrupts first
-        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()].sort(key=lambda x: x.deadline)
+        interrupts = [task for task in self.task_set.get_all() if task.is_interrupt()]
+        interrupts.sort(key=lambda x: x.deadline)
         # get periodic tasks by rate monotonic
-        periodic = [task for task in self.task_set.get_all() if not task.is_interrupt() and task.period != 0].sort(key=lambda x: 1/x.period, reverse=True)
+        periodic = [task for task in self.task_set.get_all() if not task.is_interrupt() and task.period != 0]
+        periodic.sort(key=lambda x: 1/x.period, reverse=True)
         # get other tasks
-        others = [task for task in self.task_set.get_all() if not task.is_interrupt() and task.period == 0].sort(key=lambda x: x.deadline)
+        others = [task for task in self.task_set.get_all() if not task.is_interrupt() and task.period == 0]
+        others.sort(key=lambda x: x.deadline)
         # set new tasks
         self.task_set.set_tasks(interrupts+periodic+others)
